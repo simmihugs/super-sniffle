253f17169d20b4b28da657e73f57e114dd211c43
simmihugs <simon.graetz@gmx.de>
2024-02-25 19:00:00 +0100

    added start-stop button

--- src/lib/Mug.svelte
+++ src/lib/Mug.svelte
@@ -1,18 +1,51 @@
 <script>
   import { invoke } from '@tauri-apps/api/tauri'
+  import { onMount } from 'svelte'
   
-  let fill = false;
-  let percent = 0
+
+  const fill = {
+    Dont: 0, Up: 1, Paused: 2,
+  }
+  
+  let time = new Date();
+  $: percent = 0
+  $: state = fill.Dont
+  $: seconds = time.getSeconds();
+  $: {
+    seconds = 1
+    if (state == fill.Up) {
+      percent += seconds/10
+      if (percent >= 100) {
+	state = fill.Dont
+      }
+    }
+  }
   
   function console_log(stuff) {
     invoke('log_it', { message: stuff })
   }
   
   function start_stop() {
-    fill = !fill
-    console_log(fill ? "true" : "false")
+    if (state == fill.Up) {
+      state = fill.Paused
+    } else if (state == fill.Paused) {
+      state = fill.Dont
+      percent = 0
+    } else {
+      state = fill.Up
+    }
   }
 
+  onMount(() => {
+    const interval = setInterval(() => {
+      time = new Date();
+    }, 50);
+    
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  
 </script>
 
 <div class="rows">
@@ -26,7 +59,8 @@
   </div>
   <div class="conti">  
     <button
-      on:click={start_stop}>click me</button>
+      on:click={start_stop}>{ state == fill.Up ? "Stop" : state ==
+      fill.Dont ? "Start" : "Reset" }</button>
   </div>
 </div>  
 
