93114c410144f0e70c6b612ac8a970c5bbb86eb9
simmihugs <simon.graetz@gmx.de>
2024-02-25 17:52:53 +0100

    added coffe cup

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,9 @@
 <script>
-  import Circle from './lib/Circle.svelte'
-  import Table from './lib/Table.svelte'
+  import Mug from './lib/Mug.svelte'
 
-  import { invoke } from '@tauri-apps/api/tauri'
-
-  let my_input = ''
-  function console_log(stuff) {
-    invoke('log_it', {message: stuff})
-  }
-
-  function spill_it() {
-    console_log(my_input)
-  }
-
-  function echo_it() {
-    invoke('echo_it', {message: 'it'})
-  }
-
-  let greetInputEl = ''
-  let greetMsgEl = ''
-  async function greet() {
-    greetMsgEl = await invoke("greet", { name: greetInputEl})
-  }
 </script>
 
 <main class="container">
-  <h1>Example tauri app</h1>
-
-  <div class='row'>
-    <button on:click={spill_it}>log it</button>    
-    <input id="greet-input" placeholder="Enter a name..."
-	   bind:value="{my_input}" />
-  </div>
-
-  <div class='row'>
-    <button on:click={echo_it}>echo it</button>    
-  </div>
-
-  <div class='row'>
-    <button on:click={greet}>cool</button>    
-    <input id="greet-input" placeholder="Enter a name..."
-	   bind:value="{greetInputEl}" />
-    <p>{greetMsgEl}</p>
-  </div>
-  
-  <Circle />
-  <Table />
+  <Mug />
 </main>
 
---
+++ src/lib/Mug.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { invoke } from '@tauri-apps/api/tauri'
+  
+  let fill = false;
+  let percent = 0
+  
+  function console_log(stuff) {
+    invoke('log_it', { message: stuff })
+  }
+  
+  function start_stop() {
+    fill = !fill
+    console_log(fill ? "true" : "false")
+  }
+
+</script>
+
+<div class="rows">
+  <div>
+    <div class="mug">
+      <div class="coffee"
+	   style:transform="translateY({100 - percent}%)"/>
+    </div>  
+    <div class="handle"/>
+    <div class="platform"/>
+  </div>
+  <div class="conti">  
+    <button
+      on:click={start_stop}>click me</button>
+  </div>
+</div>  
+
